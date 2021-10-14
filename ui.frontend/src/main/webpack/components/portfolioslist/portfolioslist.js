c961934817e3baebc7bbfed6c912b3377d877365
anuj_khanna <anuj7700@gmail.com>
2021-10-14 17:45:16 -0400

    Added input hidden element to save content fragment path

--- ui.frontend/src/main/webpack/components/portfolioslist/portfolioslist.js
+++ ui.frontend/src/main/webpack/components/portfolioslist/portfolioslist.js
@@ -7,35 +7,39 @@ jQuery(function ($) {
 
     function display(){
         var fragmentPath = $("input[name='portfolioslistpath']").val();
-        console.log("fragment Path " + fragmentPath);
-        var data1 = encodeURIComponent(fragmentPath);
-        var URL = location.href + "graphql/execute.json/wknd/getportfoliosbypath-query;apath=" + data1;
-        //var URI = "http://localhost:4502/graphql/execute.json/wknd/getportfoliosbypath-query;apath=" + data1;
-        
-        $.ajax({
-            url: URL,
-            dataType: "json",
-            type: "GET",
-            headers: {
-                "authorization" : "Basic YWRtaW46YWRtaW4="
-            },
-            
-            success: function(response){
-                
-                var str="<div>"
-                console.log(response);
-                var pfs = response.data.portfolioListByPath.item.includedPortfolios;
-                $(pfs).each(function(item){
-                    var portfolio = pfs[item];
-                    str = str + "<div><span>" + portfolio.description + "</span>";
-                    str = str + "<img src='" + portfolio.portfolioImage._path + "'/>";
-                    str = str + "</div>";
-                        
-                })
-                str = str + "</div>";
-                $(".portfolioslist").append(str);
-            }
-          });
+        console.log("fragment Path1 " + fragmentPath);
+        if(fragmentPath !== undefined){
+          var data1 = encodeURIComponent(fragmentPath);
+          var URL = "/graphql/execute.json/wknd/getportfoliosbypath-query;apath=" + data1;
+          console.log(URL);
+          //var URI = "http://localhost:4502/graphql/execute.json/wknd/getportfoliosbypath-query;apath=" + data1;
+          
+          $.ajax({
+              url: URL,
+              dataType: "json",
+              type: "GET",
+              headers: {
+                  "authorization" : "Basic YWRtaW46YWRtaW4="
+              },
+              
+              success: function(response){
+                  
+                  var str="<div>"
+                  console.log(response);
+                  var pfs = response.data.portfolioListByPath.item.includedPortfolios;
+                  $(pfs).each(function(item){
+                      var portfolio = pfs[item];
+                      str = str + "<div><span>" + portfolio.description + "</span>";
+                      str = str + "<img src='" + portfolio.portfolioImage._path + "'/>";
+                      str = str + "</div>";
+                          
+                  })
+                  str = str + "</div>";
+                  $(".portfolioslist").append(str);
+              }
+            });
+  
+        }
 
         /*var data = JSON.stringify({"query":"{\n  portfolioListByPath(_path: \"/content/dam/wknd/global/en/portolios/portfolios-list-home\"){\n  \titem{\n      includedPortfolios {\n        _path\n        _variation\n        portfolioLogo{\n          ... on ImageRef {\n            _path\n        \t}\n        }\n        portfolioImage{\n          ... on ImageRef {\n            _path\n        \t}\n\t\t\t\t}\n        description\n      }\n    }\n  }    \n}","variables":null});
           
